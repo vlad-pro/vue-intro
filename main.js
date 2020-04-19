a33a46343c1a1077f88b6f41cc7a0dc8235ce862
vproskurin <vladimir.proskurin@gmail.com>
2020-04-18 20:32:07 -0700

    New updates

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks',
-    image: './vmSocks-green-onWhite.jpg',
+    brand: "Yamaha",
+    product: 'YZF-R1M',
+    image: './assets/r1m.jpg',
     link: 'example.com',
     inStock: true,
-    inventory: 0,
-    onSale: false
+    inventory: 5,
+    onSale: false,
+    details: ["Best bike on the planet","200+ hp","electronic everything"],
+    colorVariants: [
+      {
+        colorId: 2234,
+        vColor: "blue",
+        vImage: "./assets/r1blue.jpg"
+      },
+      {
+        colorId: 2235,
+        vColor: "red",
+        vImage: "./assets/r1red.jpg"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    }
   }
 })
