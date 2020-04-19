d1c2dd6e5d4a3cbad7db8e1b77f4d70a1ba8fb81
vproskurin <vladimir.proskurin@gmail.com>
2020-04-18 18:12:31 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './vmSocks-green-onWhite.jpg',
+    link: 'example.com',
+    inStock: true,
+    inventory: 0,
+    onSale: false
+  }
+})
